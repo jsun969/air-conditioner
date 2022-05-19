7eef5f20d614a7f9be8f95fcfd91d41c942fa02e
Justin Sun <i@jsun969.cn>
2022-05-19 20:53:14 +0800

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:prettier/recommended', 'next/core-web-vitals'],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+  },
+};
