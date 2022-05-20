dc1dcb22fb5fc054cc81f7b6c2e88b2025394b68
Justin Sun <i@jsun969.cn>
2022-05-20 21:23:37 +0800

    feat: add wind and power fade animation

--- components/AirConditioner/AirConditioner.tsx
+++ components/AirConditioner/AirConditioner.tsx
@@ -2,6 +2,21 @@ import clsx from 'clsx';
 import React from 'react';
 import digitalFontStyles from '../../styles/digital-font.module.css';
 
+const Wind: React.FC<{ show: boolean }> = ({ show }) => {
+  return (
+    <div
+      className={clsx(
+        'flex justify-center gap-28 mt-6 transition-opacity duration-700',
+        show ? 'opacity-100' : 'opacity-0',
+      )}
+    >
+      <div className="h-16 w-1 bg-gray-300 rotate-12" />
+      <div className="h-16 w-1 bg-gray-300" />
+      <div className="h-16 w-1 bg-gray-300 -rotate-12" />
+    </div>
+  );
+};
+
 interface AirConditionerProps {
   mode: 'cool' | 'heat';
   temperature: number;
@@ -14,20 +29,25 @@ const AirConditioner: React.FC<AirConditionerProps> = ({
   power,
 }) => {
   return (
-    <div className="bg-white h-44 shadow relative border rounded-t-xl rounded-b-3xl">
-      <div className="border absolute bottom-8 w-full" />
-      <div className="absolute top-6 right-6">
-        <h6 className="text-sm mb-2">{mode === 'cool' ? '❄️' : '☀️'}</h6>
-        <h4 className={clsx('text-gray-300 text-4xl', digitalFontStyles.font)}>
-          {temperature}°C
-        </h4>
+    <div className="my-4">
+      <div className="bg-white h-44 shadow relative border rounded-t-xl rounded-b-3xl">
+        <div className="border absolute bottom-8 w-full" />
+        <div className="absolute top-6 right-6">
+          <h6 className="text-sm mb-2">{mode === 'cool' ? '❄️' : '☀️'}</h6>
+          <h4
+            className={clsx('text-gray-300 text-4xl', digitalFontStyles.font)}
+          >
+            {temperature}°C
+          </h4>
+        </div>
+        <div
+          className={clsx(
+            'absolute h-1 w-1 rounded-full bottom-6 right-2 transition-colors duration-700',
+            power ? 'bg-green-400' : 'bg-gray-200',
+          )}
+        />
       </div>
-      <div
-        className={clsx(
-          'absolute h-1 w-1 rounded-full bottom-6 right-2',
-          power ? 'bg-green-400' : 'bg-gray-200',
-        )}
-      />
+      <Wind show={power} />
     </div>
   );
 };
