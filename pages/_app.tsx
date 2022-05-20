84c11b12ecd5eb9b317510748a1529518ba439c1
Justin Sun <i@jsun969.cn>
2022-05-20 16:38:55 +0800

    refactor: rename tailwind css file name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import '../styles/globals.css';
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
