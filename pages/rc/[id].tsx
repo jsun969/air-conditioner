e720655d3420686fc45a995f5b99638896191d6a
Justin Sun <i@jsun969.cn>
2022-05-22 01:13:26 +0800

    feat: add wireless remote control screen

--- pages/rc/[id].tsx
+++ pages/rc/[id].tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
@@ -5,6 +6,7 @@ import RemoteControl from '../../components/RemoteControl';
 import { socketClient } from '../../lib/socketClient';
 import { useAppDispatch, useAppSelector } from '../../states/hooks';
 import { acActions, acSelector } from '../../states/values';
+import digitalFontStyles from '../../styles/digital-font.module.css';
 
 const WirelessRC: NextPage = () => {
   const router = useRouter();
@@ -20,8 +22,26 @@ const WirelessRC: NextPage = () => {
   }, [router.isReady]);
 
   return (
-    <main className="p-2 max-w-md mx-auto">
-      <div>{ac.data.temperature}</div>
+    <main className="p-4 max-w-sm mx-auto">
+      <div className="flex justify-around items-center py-7 border rounded-xl mb-4">
+        <div
+          className={clsx(
+            'text-8xl transition-colors duration-700',
+            digitalFontStyles.font,
+            !ac.data.power && 'text-gray-400',
+          )}
+        >
+          {ac.data.temperature}
+        </div>
+        <h6
+          className={clsx(
+            'text-6xl transition-all duration-700',
+            !ac.data.power && 'grayscale',
+          )}
+        >
+          {ac.data.mode === 'cool' ? '❄️' : '☀️'}
+        </h6>
+      </div>
       <div>
         <RemoteControl />
       </div>
