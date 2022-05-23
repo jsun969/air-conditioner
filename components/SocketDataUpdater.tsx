0d48339c38ca2fda71770153083d40232284857f
Justin Sun <i@jsun969.cn>
2022-05-23 12:51:07 +0800

    perf: update data outside redux

--- components/SocketDataUpdater.tsx
+++ components/SocketDataUpdater.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useCheckClient } from '../hooks/useCheckClient';
 import { socketClient } from '../lib/socketClient';
 import { useAppDispatch } from '../states/hooks';
-import { acActions } from '../states/values';
+import { acActions, acSelector } from '../states/values';
 
 const SocketDataUpdater: React.FC = () => {
   const dispatch = useAppDispatch();
+  const ac = useSelector(acSelector);
 
   const { isClient } = useCheckClient();
 
@@ -13,6 +15,15 @@ const SocketDataUpdater: React.FC = () => {
     if (!isClient) return;
     socketClient.on('update', (data) => dispatch(acActions.setData(data)));
   }, [isClient]);
+
+  useEffect(() => {
+    if (!isClient) return;
+    if (ac.setDataLock) {
+      dispatch(acActions.unlockSetData());
+      return;
+    }
+    socketClient.emit('update', ac.id, ac.data);
+  }, [ac.data]);
 
   return <div />;
 };
--- states/values.ts
+++ states/values.ts
@@ -1,33 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { socketClient } from '../lib/socketClient';
 import { ACData, initialACData } from '../typings/ACData';
 import { RootState } from './store';
 
 export const acSlice = createSlice({
   name: 'ac',
-  initialState: { id: '', data: initialACData },
+  initialState: { id: '', data: initialACData, setDataLock: false },
   reducers: {
     setPower(state, action: PayloadAction<boolean>) {
       state.data.power = action.payload;
-      socketClient.emit('update', state.id, state.data);
     },
     setMode(state, action: PayloadAction<'cool' | 'heat'>) {
       state.data.mode = action.payload;
-      socketClient.emit('update', state.id, state.data);
     },
     increaseTemperature(state) {
       state.data.temperature++;
-      socketClient.emit('update', state.id, state.data);
     },
     decreaseTemperature(state) {
       state.data.temperature--;
-      socketClient.emit('update', state.id, state.data);
     },
     setId(state, action: PayloadAction<string>) {
       state.id = action.payload;
     },
     setData(state, action: PayloadAction<ACData>) {
       state.data = action.payload;
+      state.setDataLock = true;
+    },
+    unlockSetData(state) {
+      state.setDataLock = false;
     },
   },
 });
